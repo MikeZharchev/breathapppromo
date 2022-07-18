e0c33fcec56c75ebf4bd4eb41a5a335ef892d757
Mikhail Zharchev <mikhailzharchev@Mikhails-MacBook-Pro.local>
2022-07-18 19:32:31 +0400

    fix performance, added modal

--- script.js
+++ script.js
@@ -3,10 +3,20 @@
 const header = document.querySelector(".header--actual");
 const headerPlaceholder = document.querySelector(".header--placeholder");
 const section1 = document.querySelector(".section--1");
+const section2 = document.querySelector(".section--2");
+const section3 = document.querySelector(".section--3");
+const section4 = document.querySelector(".section--4");
 const menuBtn = document.querySelector(".btn-menu-mob");
 const menu = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
 const buttonMenuClose = document.querySelector(".btn-menu-close");
+const playButton = document.querySelector(".play-button");
+const videoOverlay = document.querySelector(".video-overlay");
+const video = document.querySelector(".video");
+const videoHtml = document.querySelector(".video-html");
+const anchor1 = document.querySelector(".anchor--see-what-type");
+const anchor2 = document.querySelector(".anchor--think-clear");
+const anchor3 = document.querySelector(".anchor--autocomplete");
 
 // import Typed from "typed.js";
 
@@ -52,8 +62,38 @@ const toggleMobileMenu = () => {
   menuBtn.classList.toggle("hidden");
 };
 
+const toggleModal = () => {
+  document.body.classList.toggle("overflow-hidden");
+  videoOverlay.classList.toggle("hidden");
+  video.classList.toggle("video-scale");
+};
+
+// MODAL
+// set the value of the “overflow” property of the body element to “hidden” whenever the modal is opened
+playButton.addEventListener("click", toggleModal);
+videoOverlay.addEventListener("click", toggleModal);
+videoHtml.addEventListener("click", toggleModal);
+
 // Event listeners
 
+anchor1.addEventListener("click", () => {
+  section2.scrollIntoView({ behavior: "smooth" });
+  console.log("hey");
+});
+anchor2.addEventListener("click", () => {
+  section3.scrollIntoView({ behavior: "smooth" });
+  console.log("hey");
+});
+anchor3.addEventListener("click", () => {
+  section4.scrollIntoView({ behavior: "smooth" });
+  console.log("hey");
+});
 menuBtn.addEventListener("click", toggleMobileMenu);
 overlay.addEventListener("click", toggleMobileMenu);
+
+// Exit modal on esc
+document.addEventListener("keypress", (e) => {
+  console.log(e.key);
+});
+
 buttonMenuClose.addEventListener("click", toggleMobileMenu);
